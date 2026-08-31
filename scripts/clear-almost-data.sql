dacb127798d2327f4c4c8906b8f11e9da4022f60
cppHusky <cpphusky@outlook.com>
2026-08-31 21:04:49 +0900

    add sql clear data scripts

---
+++ scripts/clear-almost-data.sql
@@ -0,0 +1,7 @@
+delete from users where admin=0;--keep admins
+delete from problem_state;
+delete from score_events;
+delete from game_storage;
+delete from records;
+delete from config;
+delete from notices;
---
+++ scripts/clear-progress-data.sql
@@ -0,0 +1,4 @@
+delete from problem_state;
+delete from score_events;
+delete from game_storage;
+delete from records;
