906c5f76520739efd74a4970d18f83d879ea0770
cppHusky <cpphusky@outlook.com>
2026-09-19 22:27:53 +0900

    add admin answer api for GuessNumber

---
+++ game/GuessNumber/admin.vue
@@ -0,0 +1,12 @@
+<template>
+	<p>{{ ans }}</p>
+	<button @click="get">查看答案</button>
+</template>
+<script setup>
+import {inject,ref} from "vue";
+const ans=ref("ans");
+const api=inject("admin_api");
+async function get(){
+	ans.value=await api("get");
+}
+</script>
--- game/GuessNumber/index.ts
+++ game/GuessNumber/index.ts
@@ -7,61 +7,70 @@ const content=`
 你有 7 次机会来猜出数字；如果在 7 次之内都没有猜对，那么本关将进行重置。
 `;
 export default createPlugin({
-  pid: 'GuessNumber',
-  name: 'GuessNumber',
-  label: '01',
-  unlock: true,
-  accessible: 'always',
-  description: {
-    before_solve: {
-      content,
-    },
-  },
-  scores: [
-    { id: 'GuessNumber.in5times', desc: '只用 5 次就通过本关', points: 5 },
-    { id: 'GuessNumber.in3times', desc: '只用 3 次就通过本关', points: 5 },
-    { id: 'GuessNumber.inonce', desc: '只用 1 次就通过本关', points: 5 },
-  ],
-  checker:async(ans,ctx)=>{
-    let inputTime=ctx.gameStorage.get<number>("GuessNumber.time")??0;
-    let target=ctx.gameStorage.get<number>("GuessNumber.target");
-    if(!target){
-      target=1+Math.floor(98.9999*Math.random());
-      ctx.gameStorage.set("GuessNumber.target",target);
-    }
-    const input=parseFloat(ans);
-    inputTime++;
-    ctx.gameStorage.set("GuessNumber.time",inputTime);
-    if(Math.abs(input-target)<1e-5){
-      ctx.msg(`对了，用时 ${inputTime} 回合`);
-      if(inputTime <=5){
-        ctx.award("GuessNumber.in5times");
-        if(inputTime <=3){
-          ctx.award("GuessNumber.in3times");
-          if(inputTime===1){
-            ctx.award("GuessNumber.inonce");
-          }
-        }
-      }
-      ctx.gameStorage.set("GuessNumber.time",0);
-      ctx.gameStorage.set("GuessNumber.target",1+Math.floor(98.9999*Math.random()));
-      ctx.msg(`题目已重置`);
-      return true;
-    }
-    if(input <target){
-      ctx.msg(`小了（次数：${inputTime}/7）`);
-    }
-    else if(input>target){
-      ctx.msg(`大了（次数：${inputTime}/7）`);
-    }
-    if(inputTime>=7){
-      ctx.gameStorage.set("GuessNumber.time",0);
-      ctx.gameStorage.set("GuessNumber.target",1+Math.floor(98.9999*Math.random()));
-      ctx.msg(`输入 7 次还未猜出，本轮作废。题目已重置`);
-    }
-    return false;
-  },
-  captcha:true,
-  record:true,
-  inputs:true,
+	pid: 'GuessNumber',
+	name: 'GuessNumber',
+	label: '01',
+	unlock: true,
+	accessible: 'always',
+	description: {
+		before_solve: {
+			content,
+		},
+		admin:{
+			main:"admin.vue",
+			include:["admin.vue"],
+		},
+	},
+	scores: [
+		{ id: 'GuessNumber.in5times', desc: '只用 5 次就通过本关', points: 5 },
+		{ id: 'GuessNumber.in3times', desc: '只用 3 次就通过本关', points: 5 },
+		{ id: 'GuessNumber.inonce', desc: '只用 1 次就通过本关', points: 5 },
+	],
+	checker:async(ans,ctx)=>{
+		let inputTime=ctx.gameStorage.get<number>("GuessNumber.time")??0;
+		let target=ctx.gameStorage.get<number>("GuessNumber.target");
+		if(!target){
+			target=1+Math.floor(98.9999*Math.random());
+			ctx.gameStorage.set("GuessNumber.target",target);
+		}
+		const input=parseFloat(ans);
+		inputTime++;
+		ctx.gameStorage.set("GuessNumber.time",inputTime);
+		if(Math.abs(input-target)<1e-5){
+			ctx.msg(`对了，用时 ${inputTime} 回合`);
+			if(inputTime <=5){
+				ctx.award("GuessNumber.in5times");
+				if(inputTime <=3){
+					ctx.award("GuessNumber.in3times");
+					if(inputTime===1){
+						ctx.award("GuessNumber.inonce");
+					}
+				}
+			}
+			ctx.gameStorage.set("GuessNumber.time",0);
+			ctx.gameStorage.set("GuessNumber.target",1+Math.floor(98.9999*Math.random()));
+			ctx.msg(`题目已重置`);
+			return true;
+		}
+		if(input <target){
+			ctx.msg(`小了（次数：${inputTime}/7）`);
+		}
+		else if(input>target){
+			ctx.msg(`大了（次数：${inputTime}/7）`);
+		}
+		if(inputTime>=7){
+			ctx.gameStorage.set("GuessNumber.time",0);
+			ctx.gameStorage.set("GuessNumber.target",1+Math.floor(98.9999*Math.random()));
+			ctx.msg(`输入 7 次还未猜出，本轮作废。题目已重置`);
+		}
+		return false;
+	},
+	server:(app)=>{
+		app.adminOn("get",(_,ctx)=>{
+			return ctx.gameStorage.get<number>("GuessNumber.target");
+		});
+	},
+	captcha:true,
+	record:true,
+	inputs:true,
 });
