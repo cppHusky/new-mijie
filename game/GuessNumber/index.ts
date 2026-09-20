38d2a7839cd796e6972ae55826a9daabda3d5625
cppHusky <cpphusky@outlook.com>
2026-09-20 09:59:14 +0900

    separate before_solve description to a md file

--- game/GuessNumber/index.ts
+++ game/GuessNumber/index.ts
@@ -1,11 +1,4 @@
 import createPlugin from '../../src/types';
-const content=`
-**目标数字**是一个位于 1-99 范围内的整数。你可以随意输入任何一个数字，我会告诉你这个数字比目标大了还是小了，还是正好相同。
-
-如果正好相同，那你就过关了。
-
-你有 7 次机会来猜出数字；如果在 7 次之内都没有猜对，那么本关将进行重置。
-`;
 export default createPlugin({
 	pid: 'GuessNumber',
 	name: 'GuessNumber',
@@ -14,7 +7,7 @@ export default createPlugin({
 	accessible: 'always',
 	description: {
 		before_solve: {
-			content,
+			md:"main.md",
 		},
 		admin:{
 			main:"admin.vue",
